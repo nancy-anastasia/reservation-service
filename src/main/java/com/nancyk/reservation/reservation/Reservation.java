4f1d9d667c066847baca0e2443d45e1cff2e5506
nancy-k <naku2300@student.miun.se>
2026-08-25 16:52:51 +0200

    Updated imports

--- src/main/java/com/nancyk/reservation/reservation/Reservation.java
+++ src/main/java/com/nancyk/reservation/reservation/Reservation.java
@@ -1,7 +1,19 @@
 package com.nancyk.reservation.reservation;
 
 import com.nancyk.reservation.resource.Resource;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
--- src/main/java/com/nancyk/reservation/resource/Resource.java
+++ src/main/java/com/nancyk/reservation/resource/Resource.java
@@ -1,9 +1,16 @@
 package com.nancyk.reservation.resource;
 
-import jakarta.persistence.*;
-
 import java.time.Instant;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
